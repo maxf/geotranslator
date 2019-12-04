78ec854f3e181a352179754410f48b632f7db504
Max Froumentin <github@lapin-bl.eu>
2019-12-04 17:56:05 +0000

    use elm-ui (experiment)

--- src/View.elm
+++ src/View.elm
@@ -1,119 +1,143 @@
 module View exposing (render)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Element.Region as Region
+import Html exposing (Html)
 import String exposing (fromFloat)
 import Types exposing (..)
 
 
+colour1 =
+    rgb255 0 0 42
+
+
+colour2 =
+    rgb255 0 119 151
+
+
+colour3 =
+    rgb255 66 104 185
+
+
+colour4 =
+    rgb255 238 130 28
+
+
+positionBoxStyle =
+    [ Background.color colour2
+    , padding 8
+    , spacing 5
+    , width fill
+    , Border.rounded 2
+    ]
+
+
+positionBoxLabelStyle =
+    [ Font.size 12
+    , Font.color colour4
+    ]
+
+
+mapLinkStyle =
+    [ Background.color colour4
+    , padding 10
+    , Border.rounded 5
+    ]
+
+
+switchViewStyle =
+    [ Background.color colour3
+    , padding 10
+    , Border.rounded 5
+    ]
+
+
+debugMessageStyle =
+    [ Font.color (rgba 9 9 9 1)
+    , Font.size 12
+    ]
+
+
+lonLatStyle =
+    [ spacing 20 ]
+
+
+symbolEntryStyle =
+    [ Background.color colour3
+    , padding 10
+    , Border.rounded 5
+    ]
+
+
+
 render : Model -> Html Msg
 render model =
-    div []
-        [ renderTitle model
-        , renderInputBox model
-        , renderPosDec model
-        , renderPosDms model
-        , renderPosW3w model
-        , renderMapButton model.positionDec
-        , renderSwitchViewButton model.viewType
-        , div [] [ text model.message ]
+    layout
+        [ Background.color colour1
+        , Font.color (rgba 1 1 1 1)
+        , Font.size 19
+        , Font.family [ Font.sansSerif ]
         ]
+    <|
+        column
+            [ padding 5, spacing 10, width fill ]
+            [ renderTitle model
+            , renderInputBox model
+            , renderPosDec model
+            , renderPosDms model
+            , renderPosW3w model
+            , row [ centerX ] [ renderMapButton model.positionDec ]
+            , row [ centerX ] [ renderSwitchViewButton model.viewType ]
+            , el debugMessageStyle (text model.message)
+            ]
 
 
-renderStatus : Model -> Html Msg
+renderTitle : Model -> Element Msg
+renderTitle model =
+    paragraph
+        [ Region.heading 1
+        , Font.size 32
+        ]
+    <|
+        case model.viewType of
+            FindMe ->
+                renderStatus model
+
+            FindLocation ->
+                [ text "Find a location" ]
+
+
+renderStatus : Model -> List (Element Msg)
 renderStatus model =
     case model.browserLocation of
         Waiting ->
-            h2 []
-                [ img [ id "spinner", src "images/spinner.gif", alt "please wait" ] []
-                , text "Waiting for location"
-                ]
+            [ text "Waiting for location" ]
 
         Failure error ->
-            h2 [ class "error" ] [ "Error: " ++ error |> text ]
+            [ text <| "Error: " ++ error ]
 
         Success _ ->
-            h2 []
-                [ img [ id "radar", src "images/radar.gif", alt "spinning radar" ] []
-                , text "Tracking your location"
-                ]
+            [ text "Tracking your location" ]
 
         _ ->
-            text ""
+            [ none ]
 
 
-renderTitle : Model -> Html Msg
-renderTitle model =
-    case model.viewType of
-        FindMe ->
-            div [] [ renderStatus model ]
-
-        FindLocation ->
-            h1 [] [ text "Find a location" ]
-
-
-renderSwitchViewButton : ViewType -> Html Msg
-renderSwitchViewButton viewType =
-    case viewType of
-        FindMe ->
-            div
-                [ class "switch-view-button"
-                , onClick UserClickedSetFindLocation
-                ]
-                [ span
-                    [ class "map-button-link" ]
-                    [ text "Find another location" ]
-                ]
-
-        FindLocation ->
-            div
-                [ class "switch-view-button"
-                , onClick UserClickedSetFindMe
-                ]
-                [ span
-                    [ class "map-button-link" ]
-                    [ text "Find me" ]
-                ]
-
-
-renderMapButton : Maybe PositionDec -> Html Msg
-renderMapButton pos =
-    case pos of
-        Nothing ->
-            div [ class "map-button map-button-disabled" ]
-                [ span
-                    [ class "map-button-link" ]
-                    [ "Open in Google Maps" |> text ]
-                ]
-
-        Just { lat, lon } ->
-            div [ class "map-button" ]
-                [ a
-                    [ class "map-button-link"
-                    , target "_blank"
-                    , href ("https://www.google.com/maps/search/?api=1&query=" ++ fromFloat lat ++ "," ++ fromFloat lon)
-                    ]
-                    [ "Open in Google Maps" |> text ]
-                ]
-
-
-renderInputBox : Model -> Html Msg
+renderInputBox : Model -> Element Msg
 renderInputBox model =
     case model.viewType of
         FindLocation ->
             renderFindLocationInput model
 
         FindMe ->
-            renderFindMeInput model
+            none
 
 
-renderFindMeInput : Model -> Html Msg
-renderFindMeInput model =
-    text ""
-
-
-renderFindLocationInput : Model -> Html Msg
+renderFindLocationInput : Model -> Element Msg
 renderFindLocationInput model =
     let
         inputClass =
@@ -126,108 +150,159 @@ renderFindLocationInput model =
             else
                 "invalidInput"
     in
-    div [ class "input-area" ]
-        [ input
-            [ id "input"
-            , onInput UserTyped
-            , value model.userInput
-            , class inputClass
-            , placeholder "Type anything"
+    column
+        [ spacing 15, width fill ]
+        [ Input.search
+            [ width fill, Font.color (rgba 0 0 0 1) ]
+            { onChange = UserTyped
+            , placeholder = Just (Input.placeholder [] (text "Type anything"))
+            , text = model.userInput
+            , label = Input.labelHidden "Type anything"
+            }
+        , wrappedRow
+            [ spacing 10 ]
+            [ Input.button symbolEntryStyle
+                { onPress = Just UserEnteredDegreesSymbol
+                , label = text "°"
+                }
+            , Input.button symbolEntryStyle
+                { onPress = Just UserEnteredMinutesSymbol
+                , label = text "′"
+                }
+            , Input.button symbolEntryStyle
+                { onPress = Just UserEnteredCommaSymbol
+                , label = text ","
+                }
+            , Input.button symbolEntryStyle
+                { onPress = Just UserClickedClear
+                , label = text "Clear"
+                }
             ]
-            []
-        , br [] []
-        , button [ class "symbol", onClick UserEnteredDegreesSymbol ] [ text "°" ]
-        , button [ class "symbol", onClick UserEnteredMinutesSymbol ] [ text "′" ]
-        , button [ class "symbol", onClick UserEnteredCommaSymbol ] [ text "," ]
-        , button [ class "clear", onClick UserClickedClear ] [ text "Clear" ]
-        , p [ class "example-input" ] [ text "for instance: 51.128172,-3.465142" ]
-        , p [ class "example-input" ] [ text "or: 51° 7′ 41.4192″ N, 3° 27′ 54.5112 W" ]
-        , p [ class "example-input" ] [ text "or: incomes decide bronze" ]
+        , column
+            [ Font.size 14 ]
+            [ text "for instance: 51.128172,-3.465142"
+            , text "or: 51° 7′ 41.4192″ N, 3° 27′ 54.5112 W"
+            , text "or: incomes decide bronze"
+            ]
         ]
 
 
-renderPosDec : Model -> Html Msg
+renderPosDec : Model -> Element Msg
 renderPosDec model =
     let
         content =
             case model.positionDec of
                 Nothing ->
-                    div [ class "result empty" ] [ text "" ]
+                    none
 
                 Just pos ->
-                    div [ class "result dec" ]
+                    column
+                        lonLatStyle
                         [ "Longitude: " ++ fromFloat pos.lon |> text
-                        , br [] []
                         , "Latitude: " ++ fromFloat pos.lat |> text
                         ]
     in
-    div []
-        [ span [ class "result-header" ] [ text "Decimal" ]
+    column
+        positionBoxStyle
+        [ el positionBoxLabelStyle (text "Decimal")
         , content
         ]
 
 
-renderPosW3w : Model -> Html Msg
+renderPosDms : Model -> Element Msg
+renderPosDms model =
+    let
+        lonString dms =
+            fromFloat dms.lon.degrees
+                ++ " degrees, "
+                ++ fromFloat dms.lon.minutes
+                ++ " minutes, "
+                ++ fromFloat dms.lon.seconds
+                ++ " seconds "
+                ++ (if dms.lon.direction == "W" then
+                        "West"
+
+                    else
+                        "East"
+                   )
+
+        latString dms =
+            fromFloat dms.lat.degrees
+                ++ " degrees, "
+                ++ fromFloat dms.lat.minutes
+                ++ " minutes, "
+                ++ fromFloat dms.lat.seconds
+                ++ " seconds "
+                ++ (if dms.lat.direction == "N" then
+                        "North"
+
+                    else
+                        "South"
+                   )
+
+        pos =
+            case model.positionDms of
+                Nothing ->
+                    none
+
+                Just dms ->
+                    column
+                        lonLatStyle
+                        [ paragraph [] [ "Longitude: " ++ lonString dms |> text ]
+                        , paragraph [] [ "Latitude: " ++ latString dms |> text ]
+                        ]
+    in
+    column
+        positionBoxStyle
+        [ el positionBoxLabelStyle (text "DMS")
+        , pos
+        ]
+
+
+renderPosW3w : Model -> Element Msg
 renderPosW3w model =
     let
         w3wString =
             case model.positionW3w of
                 Nothing ->
-                    div [ class "result empty" ] [ text "" ]
+                    ""
 
                 Just words ->
-                    div [ class "result w3w" ] [ String.join " " words |> text ]
+                    String.join " " words
     in
-    div []
-        [ span [ class "result-header" ] [ text "what3words" ]
-        , w3wString
+    column
+        positionBoxStyle
+        [ el positionBoxLabelStyle (text "what3words")
+        , text w3wString
         ]
 
 
-renderPosDms : Model -> Html Msg
-renderPosDms model =
-    let
-        pos =
-            case model.positionDms of
-                Nothing ->
-                    div [ class "result empty" ] [ text "" ]
+renderMapButton : Maybe PositionDec -> Element Msg
+renderMapButton pos =
+    case pos of
+        Nothing ->
+            none
 
-                Just dms ->
-                    div [ class "result dms" ]
-                        [ "Longitude: " |> text
-                        , br [] []
-                        , fromFloat dms.lon.degrees
-                            ++ " degrees, "
-                            ++ fromFloat dms.lon.minutes
-                            ++ " minutes, "
-                            ++ fromFloat dms.lon.seconds
-                            ++ " seconds "
-                            ++ (if dms.lon.direction == "W" then
-                                    "West"
+        Just { lat, lon } ->
+            newTabLink
+                mapLinkStyle
+                { url = "https://www.google.com/maps/search/?api=1&query=" ++ fromFloat lat ++ "," ++ fromFloat lon
+                , label = text "Open in Google Maps"
+                }
 
-                                else
-                                    "East"
-                               )
-                            |> text
-                        , br [] []
-                        , "Latitude: " |> text
-                        , br [] []
-                        , fromFloat dms.lat.degrees
-                            ++ " degrees, "
-                            ++ fromFloat dms.lat.minutes
-                            ++ " minutes, "
-                            ++ fromFloat dms.lat.seconds
-                            ++ " seconds "
-                            ++ (if dms.lat.direction == "N" then
-                                    "North"
 
-                                else
-                                    "South"
-                               )
-                            |> text
-                        ]
-    in
-    div []
-        [ span [ class "result-header" ] [ text "DMS" ]
-        , pos
-        ]
+renderSwitchViewButton : ViewType -> Element Msg
+renderSwitchViewButton viewType =
+    Input.button
+        switchViewStyle
+    <|
+        case viewType of
+            FindMe ->
+                { onPress = Just UserClickedSetFindLocation
+                , label = text "Find another location"
+                }
+
+            FindLocation ->
+                { onPress = Just UserClickedSetFindMe
+                , label = text "Find me"
+                }
